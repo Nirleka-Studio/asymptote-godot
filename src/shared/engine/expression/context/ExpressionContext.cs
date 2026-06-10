de16067376e1f4ba4a20bc542d9c2c8163243b09
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2026-06-10 19:19:57 +0200

    feat(expression): make context be constructed like collections

--- src/shared/engine/expression/context/ExpressionContext.cs
+++ src/shared/engine/expression/context/ExpressionContext.cs
@@ -1,16 +1,28 @@
 using System;
-using Asymptote.Shared.Engine.Expression.Tree;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Asymptote.Shared.Engine.Expression.Context;
 
 #nullable enable
-public class ExpressionContext
+public class ExpressionContext : IEnumerable<KeyValuePair<string, object?>>
 {
-    private readonly Func<string, object> _lookup;
+    private readonly Dictionary<string, object?> _variables = new();
+    private readonly Func<string, object?>? _lookup;
+
+    public ExpressionContext()
+    {
+    }
 
     public ExpressionContext(Func<string, object?> lookup)
     {
         _lookup = lookup ?? throw new ArgumentNullException(nameof(lookup));
+    }
+
+    public object? this[string key]
+    {
+        get => GetVariable(key);
+        set => _variables[key] = value;
     }
 
     public object? GetVariable(string name)
@@ -18,7 +30,12 @@ public class ExpressionContext
         if (string.IsNullOrWhiteSpace(name))
             throw new ArgumentException("Variable name cannot be null or empty", nameof(name));
 
-        var value = _lookup(name);
-        return value;
+        if (_variables.TryGetValue(name, out var value))
+            return value;
+
+        return _lookup?.Invoke(name);
     }
+
+    public IEnumerator<KeyValuePair<string, object?>> GetEnumerator() => _variables.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
