0a13da9376cd6b6a94f6d2c02d13954f3443b092
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2026-06-14 15:58:34 +0200

    feat(zone): implement zone

--- src/shared/world/level/zone/Zone.cs
+++ src/shared/world/level/zone/Zone.cs
@@ -1,4 +1,6 @@
 using Asymptote.Shared.World.Entity.AI.Detection;
+using Asymptote.Shared.World.Entity.Component;
+using Asymptote.Shared.World.Entity.Player;
 using Godot;
 
 namespace Asymptote.Shared.World.Level.Zone;
@@ -23,10 +25,33 @@ public partial class Zone : Area3D
 
     private void onBodyEntered(Node3D body)
     {
+        GD.Print($"Body entered: {body.Name}");
+        if (body is PlayerEntity playerEntity)
+        {
+            var component = playerEntity.getComponent<AccessComponent>();
+            if (component != null)
+            {
+                component.onEnterZone(this);
+            }
+        }
     }
 
     private void onBodyExited(Node3D body)
     {
+        GD.Print($"Body left: {body.Name}");
+        if (body is PlayerEntity playerEntity)
+        {
+            var component = playerEntity.getComponent<AccessComponent>();
+            if (component != null)
+            {
+                component.onLeaveZone(this);
+            }
+        }
+    }
+
+    public bool isAuthorized(PlayerEntity player)
+    {
+        return false; // TODO: FOR NOW
     }
 
     public override void _ExitTree()
