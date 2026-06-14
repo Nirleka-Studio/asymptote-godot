23ff37608a1a114bcdf8390ad999c4338f93dbf9
NirlekaPlay <93853760+NirlekaPlay@users.noreply.github.com>
2026-06-14 15:41:56 +0200

    feat(character)!: attach CharacterController.gd to a seperate node

--- src/client/character/CharacterController.gd
+++ src/client/character/CharacterController.gd
@@ -1,4 +1,4 @@
-extends CharacterBody3D
+extends Node3D
 class_name CharacterController
 
 const SPEED = 5.0
@@ -7,14 +7,15 @@ var coyote_timer := 0.0
 const COYOTE_TIME_LIMIT := 0.15 # Seconds
 const JUMP_VELOCITY_MIN := 0.6  # For short hops
 
+@onready var character: CharacterBody3D = get_parent() as CharacterBody3D
 @export var jumping := false
 
-@onready var camera_pivot: CameraController = $CameraPivot
+@onready var camera_pivot: CameraController = get_parent().find_child("CameraPivot")
 
 func _enter_tree() -> void:
 	# This is a terrible way to do this.
 	# But I have no choice since we can't bridge CS to GDScript.
-	var peer_id: int = name.to_int()
+	var peer_id: int = get_parent().name.to_int()
 	set_multiplayer_authority(peer_id)
 
 	for child in get_children():
@@ -29,22 +30,22 @@ func _physics_process(delta: float) -> void:
 	if not is_multiplayer_authority():
 		return
 
-	if is_on_floor():
+	if character.is_on_floor():
 		coyote_timer = COYOTE_TIME_LIMIT
 	else:
 		coyote_timer -= delta
 
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if not character.is_on_floor():
+		character.velocity += character.get_gravity() * delta
 
 	if Input.is_action_just_pressed("jump") and coyote_timer > 0:
-		velocity.y = JUMP_VELOCITY
+		character.velocity.y = JUMP_VELOCITY
 		jump.rpc()
 		coyote_timer = 0 # Prevent double jumping
 
 	# variable Jump Height: if player releases jump button while still going up
-	if Input.is_action_just_released("jump") and velocity.y > JUMP_VELOCITY_MIN:
-		velocity.y = JUMP_VELOCITY_MIN
+	if Input.is_action_just_released("jump") and character.velocity.y > JUMP_VELOCITY_MIN:
+		character.velocity.y = JUMP_VELOCITY_MIN
 
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	# calculate direction relative to Camera Rotation instead of Character self-transform
@@ -56,23 +57,23 @@ func _physics_process(delta: float) -> void:
 	var direction := (forward * input_dir.y + right * input_dir.x).normalized()
 
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		character.velocity.x = direction.x * SPEED
+		character.velocity.z = direction.z * SPEED
 		
 		# Only turn character body if NOT aiming or in 1st person
 		if not camera_pivot.is_first_person() and not Input.is_action_pressed("zoom_aim"):
 			var target_angle: float = atan2(-direction.x, -direction.z)
 			rotation.y = rotation.y + angle_difference(rotation.y, target_angle) * 0.15
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		character.velocity.x = move_toward(character.velocity.x, 0, SPEED)
+		character.velocity.z = move_toward(character.velocity.z, 0, SPEED)
 
 	# Force face along camera if in First Person or Aiming (Roblox CharacterController behavior)
 	if camera_pivot.is_first_person() or Input.is_action_pressed("zoom_aim"):
 		var camera_y_rot: float = camera_pivot.rot_y
 		rotation.y = camera_y_rot
 
-	move_and_slide()
+	character.move_and_slide()
 	
 @rpc("call_local")
 func jump() -> void:
